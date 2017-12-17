907a9d32f7532a95589c08d203433e2630c9ecc1
zms00@139.com <zms00@139.com>
2017-12-17 19:42:00 +0800

    SpringBoot第一个java程序

---
+++ src/main/java/com/zms/girl/GirlApplication.java
@@ -0,0 +1,15 @@
+package com.zms.girl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})//阻止自动注入dataSource bean
+public class GirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlApplication.class, args);
+	}
+}
